80b68a2d4e9bfd6b61f0386fa10b400c956d0544
Emirhan Kurban <100554100+emirhankurb4n@users.noreply.github.com>
2022-03-29 08:51:06 +0300

    Kodlar

---
+++ Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication22
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            maskedTextBox1.Mask = "00000000000";
+            maskedTextBox2.Mask = "(999) 000 00 00";
+            maskedTextBox3.Mask = "00/00/0000";
+            maskedTextBox4.Mask = "0000-0000-0000-0000";
+            maskedTextBox5.Mask = "###.###.###.###";
+        }
+    }
+}
